b67c2dc178e734f3c196655d5439b7b623228a0b
external-reality <ecjones2040@gmail.com>
2013-07-15 03:51:08 -0400

    Remove unused imports and whitespace cleanup.

--- src/PotionMaker.hs
+++ src/PotionMaker.hs
@@ -2,18 +2,17 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module PotionMaker where
 
 ------------------------------------------------------------------------------
 import Authentication.AcidStateBackend
 import Control.Lens hiding (Indexable)
-import Control.Monad
 import Data.Data
 import Data.IxSet
 import Data.Set
 import Data.SafeCopy
-import Reagent
 import Snap.Snaplet.Auth
 ------------------------------------------------------------------------------
 import Mixture
@@ -23,7 +22,6 @@ type PotionMakerId = UserId
 
 deriving instance Typeable PotionMakerId
 deriving instance Data PotionMakerId
-
 
 ------------------------------------------------------------------------------
 data PotionMaker = PotionMaker { _potionMakerId :: PotionMakerId               
