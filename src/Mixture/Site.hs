d9357edcaf5ef04c07f71d595044a486cc723b09
external-reality <ecjones2040@gmail.com>
2013-07-15 03:50:30 -0400

    Ensure mixtures get decoded unvalidated and remove unused imports.

--- src/Mixture/Site.hs
+++ src/Mixture/Site.hs
@@ -2,25 +2,15 @@
 
 module Mixture.Site where
 
-import           Authentication.AcidStateBackend
-import           Control.Monad (liftM, (=<<))
-import           Control.Monad.Trans
+import           Control.Monad (liftM)
 import           Data.Aeson
 import           Data.ByteString (ByteString)
-import           Data.Maybe
-import qualified Data.Text as T
-import qualified Heist.Interpreted as I
 import           Snap.Core
 import           Snap.Snaplet
 import           Snap.Snaplet.AcidState
 import           Snap.Snaplet.Auth
-import           Snap.Snaplet.Heist
-import           Data.Set
 ------------------------------------------------------------------------------
 import           Application
-import           Mixture
-import           PotionMaker
-
 
 ------------------------------------------------------------------------------
 saveMixture :: Handler App (AuthManager App) ()
@@ -28,20 +18,19 @@ saveMixture = method PUT $ do
   maybeMixture <- liftM decode $ readRequestBody 2048 
   case maybeMixture of
     (Just mixture) -> do
-      maybeValidMixture <- query $ ValidateMixture mixture
-      case maybeValidMixture of 
-        (Just validMixture) -> saveMixtureForCurrentUser validMixture
-        Nothing             -> modifyResponse $ setResponseStatus 400 ""
-    Nothing -> modifyResponse $ setResponseStatus 400 ""
+      eitherErrorMixture <- query $ ValidateMixture mixture
+      case eitherErrorMixture of 
+        (Right validMixture) -> saveMixtureForCurrentUser validMixture
+        (Left e)             -> modifyResponse $ setResponseStatus 400 e
+    Nothing -> modifyResponse $ setResponseStatus 400 "Invalid Data"
 
   where
     saveMixtureForCurrentUser mixture = do
       maybeUserId <- liftM (userId =<<) currentUser
       case maybeUserId of   
-        (Just id) -> update $ SaveMixture mixture id
-        Nothing   -> modifyResponse $ setResponseStatus 500 ""
+        (Just uId) -> update $ SaveMixture mixture uId
+        Nothing   -> modifyResponse $ setResponseStatus 500 "Invalid Request"
 
 ------------------------------------------------------------------------------
 routes :: [(ByteString, Handler App (AuthManager App) ())]
 routes = [ ("api/mixtures", saveMixture) ]
-          
