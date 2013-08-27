19f0184da37e037d6b292c89ff539dd78b7bc151
external-reality <ecjones2040@gmail.com>
2013-08-27 00:39:19 -0400

    Add file for testing out sunroof compilation.

---
+++ src/JavaScript/RouterUtilties.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+
+module JavaScript.RouterUtilities (replaceContentWith) where
+
+-----------------------------------------------------------------------------------------
+import Control.Monad
+import Data.Boolean
+import Data.Default
+import Language.Sunroof
+import Language.Sunroof.JS.JQuery
+------------------------------------------------------------------------------
+import JavaScript.Backbone.View
+
+------------------------------------------------------------------------------
+replaceOrAppend :: JSObject -> JSObject -> JSA ()
+replaceOrAppend content replacement = do
+  innerContent  <- content # children
+  let isContentEmpty = (innerContent ! jsLength)  ==* (0 :: JSNumber)
+  ifB isContentEmpty
+     (append content replacement)
+     (void $ replaceWith innerContent replacement)
+
+------------------------------------------------------------------------------
+replaceContentWith :: JSBackboneView NotRendered -> JSA ()
+replaceContentWith view = do
+  content <- jQuery "#content"
+  element <- render view 
+  replaceOrAppend content $ el element
+
+------------------------------------------------------------------------------
+children :: JSObject -> JS t JSObject
+children = invoke "children" ()
+
+------------------------------------------------------------------------------
+replaceWith :: JSObject -> JSObject -> JS t JSObject
+replaceWith = invoke "replaceWith"
+
+------------------------------------------------------------------------------
+jsLength :: JSSelector JSNumber
+jsLength = attr "length"
+
+------------------------------------------------------------------------------
+
